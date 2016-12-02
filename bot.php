9ff1e3aa620d152e128b414e7c761c00f028c598
insidez <auckarapon35441n@gmail.com>
2016-12-02 14:02:55 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -22,9 +22,7 @@ if (!is_null($events['events'])) {
 			
 			$messages = [
 				{'type' => 'text'
-				,'text' => $text}
-				,{'type' => 'text'
-				,'text' => $text.'2'}
+				,'text' => $text.$checkText}
 			];
 			
 			$url = 'https://api.line.me/v2/bot/message/reply';
