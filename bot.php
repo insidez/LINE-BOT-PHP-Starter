e9b45419a0060985977cccd12bcd6e88fd18266a
insidez <auckarapon35441n@gmail.com>
2016-12-02 13:59:27 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -25,7 +25,7 @@ if (!is_null($events['events'])) {
 				,'text' => $text}
 				
 				,{'type' => 'text'
-				,'text' => $text.'2'},
+				,'text' => $text.'2'}
 			];
 			
 			$url = 'https://api.line.me/v2/bot/message/reply';
